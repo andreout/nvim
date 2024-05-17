9ec40fa756ee437de92e4962441e1535d1c06849
André Outeiro <andre@outeiro.com>
2024-05-17 19:29:02 +0200

    dashboard ssb

---
+++ lua/plugins/dashboard.ssb.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "nvimdev/dashboard-nvim",
+    event = "VimEnter",
+    opts = function(_, opts)
+      local logo = [[
+    ███████   ██████   ██████ 
+    ██        ██   ██  ██   ██
+    ███████   ███████  ███████
+         ██   ██   ██  ██   ██
+    ███████   ██████   ██████
+
+      ]]
+      logo = string.rep("\n", 4) .. logo .. "\n"
+      opts.config.header = vim.split(logo, "\n")
+    end,
+  },
+}
