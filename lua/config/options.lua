8838ce8a3d25e99d441a9a2c23b571765b1bd47a
André Outeiro <andre@outeiro.com>
2024-08-13 00:22:59 +0200

    feat: linhas longas visualizadas completas

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,9 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
+-- Add any additional oplocal opt = vim.opt
+
+-- Activar el ajuste de línea
+local opt = vim.opt
+
+opt.wrap = true
+opt.linebreak = true -- Opcional, para ajustar líneas en palabras completastions here
